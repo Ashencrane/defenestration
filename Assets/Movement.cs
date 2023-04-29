fe999e7d96a1d11b4b8d9cd82120212ce67d82ec
Michael DeVera <michael.devera0000@gmail.com>
2023-04-29 00:38:11 -0700

    refined movement and visual indicators

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -11,28 +11,32 @@ public class Movement : MonoBehaviour
     TMP_Text healthDisplay;
     Movement otherMoveScript;
     SpriteRenderer spr;
+    double backDashSec = 0;
 
     public bool P1;
     public int health;
 
 
-    const float MOVE_SPEED = 5f;
+    const float MOVE_SPEED = 4.5f;
 
     const float NONE_ATTACK_KNOCKBACK = 30f;
 
     const float LIGHT_ATTACK_STARTUP = 0.1f;
-    const float LIGHT_ATTACK_ACTIVE = 0.3f;
-    const float LIGHT_ATTACK_RECOVERY = 0.1f;
-    const float LIGHT_ATTACK_DISTANCE = 0.6f;
-    const float LIGHT_ATTACK_STUN = 0.2f;
-    const float LIGHT_ATTACK_KNOCKBACK = 100f;
+    const float LIGHT_ATTACK_ACTIVE = 0.2f;
+    const float LIGHT_ATTACK_RECOVERY = 0.15f;
+    const float LIGHT_ATTACK_DISTANCE = 1.1f;
+    const float LIGHT_ATTACK_STUN = 0.25f;
+    const float LIGHT_ATTACK_KNOCKBACK = 180f;
 
     const float HEAVY_ATTACK_STARTUP = 0.3f;
-    const float HEAVY_ATTACK_ACTIVE = 0.5f;
-    const float HEAVY_ATTACK_RECOVERY = 0.5f;
-    const float HEAVY_ATTACK_DISTANCE = 0.9f;
-    const float HEAVY_ATTACK_STUN = 0.4f;
-    const float HEAVY_ATTACK_KNOCKBACK = 200f;
+    const float HEAVY_ATTACK_ACTIVE = 0.3f;
+    const float HEAVY_ATTACK_RECOVERY = 0.7f;
+    const float HEAVY_ATTACK_DISTANCE = 1.6f;
+    const float HEAVY_ATTACK_STUN = 0.45f;
+    const float HEAVY_ATTACK_KNOCKBACK = 280f;
+
+    const float BACKDASH_FORCE = 320f;
+    const float BACKDASH_RECOVERY = 0.5f;
 
     public enum Attack
     {
@@ -83,19 +87,44 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(backDashSec > 0)
+        {
+            backDashSec -= Time.deltaTime;
+        }
+
+
+
 
         if(actionable)
         {
+            //backdash code
 
-            int lr = P1 ? (int)Input.GetAxisRaw("HorizontalP1") : (int)Input.GetAxisRaw("HorizontalP2");
+            if (Input.GetKeyDown(KeyCode.A) && P1 || Input.GetKeyDown(KeyCode.RightArrow) && !P1) //scuffed but works
+            {
+                if (backDashSec > 0)
+                {
+                    StartCoroutine("Backdash");
+                    backDashSec = 0;
+                }
+                else
+                {
+                    backDashSec = 0.25f;
+                }
+            }
+
+
+
+
+
+            int leftright = P1 ? (int)Input.GetAxisRaw("HorizontalP1") : (int)Input.GetAxisRaw("HorizontalP2");
             
 
-            if (lr == -1)
+            if (leftright == -1)
             {
                 gameObject.transform.Translate(new Vector2(-MOVE_SPEED, 0) * Time.deltaTime);
+
             }
-            else if (lr == 1)
+            else if (leftright == 1)
             {
                 gameObject.transform.Translate(new Vector2(MOVE_SPEED, 0) * Time.deltaTime);
             }
@@ -113,6 +142,17 @@ public class Movement : MonoBehaviour
 
         }
     }
+    IEnumerator Backdash()
+    {
+        actionable = false;
+        rb2d.AddForce(new Vector2(-BACKDASH_FORCE * direction, 0));
+        spr.color = new Color(0.7f, 0.7f, 0.7f);
+        yield return new WaitForSeconds(BACKDASH_RECOVERY);
+        spr.color = new Color(200, 200, 200);
+        actionable = true;
+
+        yield return null;
+    }
 
     IEnumerator LightAttack()
     {
@@ -120,12 +160,12 @@ public class Movement : MonoBehaviour
         actionable = false;
         yield return new WaitForSeconds(LIGHT_ATTACK_STARTUP);
         sword.transform.Translate(new Vector2(LIGHT_ATTACK_DISTANCE * direction, 0)); //stab extend
-
+        spr.color = new Color(0.7f, 0.7f, 0.7f);
         yield return new WaitForSeconds(LIGHT_ATTACK_ACTIVE);
         sword.transform.Translate(new Vector2(-LIGHT_ATTACK_DISTANCE * direction, 0));
 
         yield return new WaitForSeconds(LIGHT_ATTACK_RECOVERY);
-
+        spr.color = new Color(200, 200, 200);
         currentAttack = Attack.None;
         actionable = true;
 
@@ -136,18 +176,18 @@ public class Movement : MonoBehaviour
     {
         currentAttack = Attack.Heavy;
         actionable = false;
-        spr.color = new Color(255, 0, 0);
+        spr.color = new Color(0.3f, 0.3f, 1f);
 
         yield return new WaitForSeconds(HEAVY_ATTACK_STARTUP);
-        spr.color = new Color(200, 200, 200);
+        
         sword.transform.Translate(new Vector2(HEAVY_ATTACK_DISTANCE * direction, 0)); //stab extend
-
+        spr.color = new Color(0.7f, 0.7f, 0.7f);
         yield return new WaitForSeconds(HEAVY_ATTACK_ACTIVE);
         sword.transform.Translate(new Vector2(-HEAVY_ATTACK_DISTANCE * direction, 0));
-
-        yield return new WaitForSeconds(HEAVY_ATTACK_RECOVERY * direction);
-
         
+        yield return new WaitForSeconds(HEAVY_ATTACK_RECOVERY * direction);
+        spr.color = new Color(200, 200, 200);
+
         currentAttack = Attack.None;
         actionable = true;
 
@@ -182,10 +222,10 @@ public class Movement : MonoBehaviour
         health -= 1;
         healthDisplay.text = "Health: " + health.ToString();
         rb2d.AddForce(new Vector2(-LIGHT_ATTACK_KNOCKBACK * direction, 0));
+        spr.color = new Color(255, 0, 0);
 
-        
         yield return new WaitForSeconds(LIGHT_ATTACK_STUN);
-
+        spr.color = new Color(200, 200, 200);
         actionable = true;
         yield return null;
     }
@@ -196,10 +236,10 @@ public class Movement : MonoBehaviour
         health -= 2;
         healthDisplay.text = "Health: " + health.ToString();
         rb2d.AddForce(new Vector2(-HEAVY_ATTACK_KNOCKBACK * direction, 0));
-
+        spr.color = new Color(255, 0, 0);
 
         yield return new WaitForSeconds(HEAVY_ATTACK_STUN);
-
+        spr.color = new Color(200, 200, 200);
         actionable = true;
         yield return null;
     }
@@ -208,10 +248,10 @@ public class Movement : MonoBehaviour
         actionable = false;
         health -= 1;
         rb2d.AddForce(new Vector2(-NONE_ATTACK_KNOCKBACK * direction, 0));
-
+        spr.color = new Color(255, 0, 0);
 
         yield return new WaitForSeconds(LIGHT_ATTACK_STUN);
-
+        spr.color = new Color(200, 200, 200);
         actionable = true;
         yield return null;
     }
