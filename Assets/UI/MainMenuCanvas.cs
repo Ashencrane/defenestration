cd6e02d92efff50d780981a636b8389feffc454c
Dev Patel <devp2@uci.edu>
2023-05-05 17:58:52 -0700

    Complete Main Menu v1

--- Assets/UI/MainMenuCanvas.cs
+++ Assets/UI/MainMenuCanvas.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+struct MainMenuSettings
+{
+    public static float MusicSetting = 1;
+    public static float SfxSetting = 1;
+}
+
 public class MainMenuCanvas : MonoBehaviour
 {
-    [SerializeField] private string LevelName;
+    [SerializeField] private string levelName;
 
     public void OnPressStartButton()
     {
-        SceneManager.LoadScene(LevelName);
+        SceneManager.LoadScene(levelName);
     }
     
     public void OnPressQuitButton()
     {
         Application.Quit();
+    }
+
+    public void UpdateMusicSetting(float val)
+    {
+        MainMenuSettings.MusicSetting = val;
+        Debug.Log("New music volume: " + MainMenuSettings.MusicSetting);
+    }
+
+    public void UpdateSfxSetting(float val)
+    {
+        MainMenuSettings.SfxSetting = val;
+        Debug.Log("New sfx volume: " + MainMenuSettings.SfxSetting);
     }
 }
