9e3be90301e6fa0cb4cf47337b1eed03087b0a80
Dev Patel <devp2@uci.edu>
2023-05-09 13:47:03 -0700

    Victory screen and folder organization

---
+++ Assets/Scripts/UI/VictoryMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class VictoryMenu : MonoBehaviour
+{
+    [SerializeField] private string mainMenuScene = "MainMenu";
+    [SerializeField] private TMP_Text victoryText;
+
+    public void SetVictoryText(string text = "P1 Wins!")
+    {
+        victoryText.text = text;
+    }
+    
+    public void ReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
