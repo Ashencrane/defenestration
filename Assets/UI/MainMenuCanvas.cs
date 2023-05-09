f438cf2c9266edeae72c8db37f56373e1fe22b4b
Dev Patel <devp2@uci.edu>
2023-05-08 23:50:56 -0700

    Add resolution options

--- Assets/UI/MainMenuCanvas.cs
+++ Assets/UI/MainMenuCanvas.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-struct MainMenuSettings
-{
-    public static float MusicSetting = 1;
-    public static float SfxSetting = 1;
-}
 
 public class MainMenuCanvas : MonoBehaviour
 {
@@ -21,17 +16,5 @@ public class MainMenuCanvas : MonoBehaviour
     public void OnPressQuitButton()
     {
         Application.Quit();
-    }
-
-    public void UpdateMusicSetting(float val)
-    {
-        MainMenuSettings.MusicSetting = val;
-        Debug.Log("New music volume: " + MainMenuSettings.MusicSetting);
-    }
-
-    public void UpdateSfxSetting(float val)
-    {
-        MainMenuSettings.SfxSetting = val;
-        Debug.Log("New sfx volume: " + MainMenuSettings.SfxSetting);
     }
 }
